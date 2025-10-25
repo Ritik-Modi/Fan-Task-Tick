4ae5f74c78d4a45774fe113c99bbac735d499a6f
Ritik Modi <ritikmodi@Ritiks-MacBook-Air.local>
2025-10-25 06:21:02 +0530

    .

--- server/utils/mailSender.util.js
+++ server/utils/mailSender.util.js
@@ -46,7 +46,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 async function mailSender(email, title, body) {
   try {
     const data = await resend.emails.send({
-      from: "StudyNotion <no-reply@studynotion.com>",
+      from: "Fantasktick <no-reply@Fantasktick.com>",
       to: email,
       subject: title,
       html: body,
